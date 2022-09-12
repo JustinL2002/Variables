4c14d1666f1877ce3099c6e35487d7b29e505f3a
Justin Lawson <jmlawson2002@gmail.com>
2022-09-12 13:00:26 -0400

    Variables Practice

---
+++ UsingVariablesConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UsingVariablesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 20;
+            string myName = "Justin";
+            char myNickname = 'J';
+            bool isCoding = true;
+            double tShirts = 5;
+            decimal money = 2.5m;
+
+            Console.WriteLine($"Hello, my name is {myName}, but some people call me {myNickname}, and I am {myAge} years old");
+            Console.WriteLine($"Right now, it is {isCoding} that I am coding");
+            Console.WriteLine($"I own {tShirts} shirts. Earlier today, I found {money} dollars on the ground" );
+
+            
+        }
+    }
+}
+
